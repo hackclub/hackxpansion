db2baff12771200140c51d9c280f37ad38a8905d
KOEGlike <koeglike@proton.me>
2026-08-17 13:56:20 +0300

    another migration fix

--- website/drizzle/0017_chunky_cerebro.sql
+++ website/drizzle/0017_chunky_cerebro.sql
@@ -1,1 +1,9 @@
-ALTER TYPE "public"."project_type" ADD VALUE IF NOT EXISTS 'mod';
+ALTER TYPE "public"."project_type" RENAME TO "project_type_old";--> statement-breakpoint
+CREATE TYPE "public"."project_type" AS ENUM('card', 'app', 'mod');--> statement-breakpoint
+ALTER TABLE "project" ALTER COLUMN "type" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "project" ALTER COLUMN "type" TYPE "public"."project_type" USING "type"::text::"public"."project_type";--> statement-breakpoint
+ALTER TABLE "project" ALTER COLUMN "design_approved_type" TYPE "public"."project_type" USING "design_approved_type"::text::"public"."project_type";--> statement-breakpoint
+ALTER TABLE "project" ALTER COLUMN "type" SET DEFAULT 'card';--> statement-breakpoint
+DROP TYPE "public"."project_type_old";--> statement-breakpoint
+ALTER TABLE "project" DROP CONSTRAINT "project_resistor_assignment";--> statement-breakpoint
+ALTER TABLE "project" ADD CONSTRAINT "project_resistor_assignment" CHECK (("project"."type" IN ('app', 'mod') AND "project"."md0" IS NULL AND "project"."md1" IS NULL) OR ("project"."type" = 'card' AND "project"."md0" IS NOT NULL AND "project"."md1" IS NOT NULL));
